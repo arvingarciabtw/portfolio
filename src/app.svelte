d6637f2294a2551020d2b72df7d100c8744f572e
Arvin Garcia <arvin@arvingarcia.com>
2026-04-02 04:06:36 +0800

    refactor: isolate project into a component

--- src/app.svelte
+++ src/app.svelte
@@ -1,8 +1,9 @@
 <script>
   import Divider from "./components/divider.svelte";
+  import Project from "./components/project.svelte";
 </script>
 
-<div class="wrapper">
+<main class="wrapper">
   <section class="hero">
     <div class="info">
       <h1 class="heading">Arvin Garcia</h1>
@@ -58,163 +59,46 @@
     <p>26 - TODAY</p>
   </div>
 
-  <Divider />
+  <section class="projects">
+    <Divider />
 
-  <section>
-    <section class="project">
-      <div class="top">
-        <a
-          class="redirect"
-          href="https://fractal.arvingarcia.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 class="name">Fractal</h1>
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg
-          >
-        </a>
-        <p class="year">2026</p>
-      </div>
-      <div class="middle">
-        <div class="img-placeholder"></div>
-        <div class="pills"></div>
-      </div>
-      <div class="bottom">
-        <div class="description">
-          A programmer-centric spaced repetition web application with native
-          syntax highlighting and an algorithm for faster card reviews.
-        </div>
-        <a href="/">&gt;&gt; Read Case Study</a>
-      </div>
-    </section>
+    <Project
+      url="https://fractal.arvingarcia.com"
+      name="Fractal"
+      year="2026"
+      description="A programmer-centric spaced repetition web application with native syntax highlighting and an algorithm for faster card reviews."
+      route="/"
+    />
 
     <Divider />
-    <section class="project">
-      <div class="top">
-        <a
-          class="redirect"
-          href="https://syntype.arvingarcia.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 class="name">Syntype</h1>
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg
-          >
-        </a>
-        <p class="year">2026</p>
-      </div>
-      <div class="middle">
-        <div class="img-placeholder"></div>
-        <div class="pills"></div>
-      </div>
-      <div class="bottom">
-        <div class="description">
-          A practical typing test where you type actual code blocks, instead of
-          a random assortment of words.
-        </div>
-        <a href="/">&gt;&gt; Read Case Study</a>
-      </div>
-    </section>
+
+    <Project
+      url="https://syntype.arvingarcia.com"
+      name="Syntype"
+      year="2026"
+      description="A practical typing test where you type actual code blocks, instead of a random assortment of words."
+      route="/"
+    />
 
     <Divider />
-    <section class="project">
-      <div class="top">
-        <a
-          class="redirect"
-          href="https://monad.arvingarcia.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 class="name">Monad</h1>
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg
-          >
-        </a>
-        <p class="year">2026</p>
-      </div>
-      <div class="middle">
-        <div class="img-placeholder"></div>
-        <div class="pills"></div>
-      </div>
-      <div class="bottom">
-        <div class="description">
-          A personal design system for building user interfaces which supports
-          Svelte and React.
-        </div>
-        <a href="/">&gt;&gt; Read Case Study</a>
-      </div>
-    </section>
+
+    <Project
+      url="https://monad.arvingarcia.com"
+      name="Monad"
+      year="2026"
+      description="A personal design system for building user interfaces which supports Svelte and React."
+      route="/"
+    />
 
     <Divider />
-    <section class="project">
-      <div class="top">
-        <a
-          class="redirect"
-          href="https://labs.arvingarcia.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 class="name">Labs</h1>
-          <svg
-            class="icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg
-          >
-        </a>
-        <p class="year">2026</p>
-      </div>
-      <div class="middle">
-        <div class="img-placeholder"></div>
-        <div class="pills"></div>
-      </div>
-      <div class="bottom">
-        <div class="description">
-          A collection of experiments on my personal interests on 3d graphics
-          and shaders.
-        </div>
-        <a href="/">&gt;&gt; Read Case Study</a>
-      </div>
-    </section>
+
+    <Project
+      url="https://labs.arvingarcia.com"
+      name="Labs"
+      year="2026"
+      description="A collection of experiments on my personal interests on 3d graphics and shaders."
+      route="/"
+    />
 
     <Divider />
   </section>
@@ -238,7 +122,7 @@
       </li>
     </ul>
   </footer>
-</div>
+</main>
 
 <style>
   p {
@@ -335,72 +219,6 @@
       right: -2px;
     }
   }
-  .project {
-    padding: 3rem;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-  }
-  .project .redirect {
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    text-decoration: none;
-    cursor: pointer;
-  }
-  .project .name {
-    font-size: 14px;
-    font-weight: 400;
-  }
-  .project .icon {
-    height: 18px;
-  }
-  .project .top {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-family: "departure-mono", monospace;
-    text-transform: uppercase;
-  }
-  .project .year {
-    font-family: "departure-mono", monospace;
-  }
-  .project .middle {
-    width: 100%;
-  }
-  .project .img-placeholder {
-    width: 100%;
-    height: 40rem;
-    background: var(--color-gray-100);
-    border: 1px dashed var(--color-gray-400);
-  }
-  .project .bottom {
-    display: grid;
-    grid-template-columns: 1fr 200px;
-    gap: 2rem;
-  }
-  .project .description {
-    max-width: 560px;
-    font-family: "source-serif-4", serif;
-    font-size: 14px;
-  }
-  .project:nth-of-type(2) .description {
-    max-width: 440px;
-  }
-  .project:nth-of-type(3) .description {
-    max-width: 400px;
-  }
-  .project:last-of-type .description {
-    max-width: 400px;
-  }
-  .project .bottom a {
-    justify-self: end;
-    align-self: center;
-    font-family: "departure-mono", monospace;
-    font-size: 14px;
-    text-transform: uppercase;
-    text-decoration: none;
-  }
   footer {
     padding: 3rem;
     display: flex;
@@ -429,9 +247,6 @@
     .hero {
       grid-template-columns: 400px 1fr;
     }
-    .project .img-placeholder {
-      height: 30rem;
-    }
   }
 
   @media (max-width: 650px) {
@@ -444,35 +259,15 @@
     .art {
       display: none;
     }
-    .project .img-placeholder {
-      height: 20rem;
-    }
-    .project .bottom {
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-    }
-    .project .bottom a {
-      align-self: start;
-    }
   }
 
   @media (max-width: 500px) {
     .projects-head p {
       display: none;
     }
-    .project .img-placeholder {
-      height: 15rem;
-    }
   }
 
   @media (max-width: 420px) {
-    .project .img-placeholder {
-      margin: 0 -3rem;
-      width: auto;
-      border-left: none;
-      border-right: none;
-    }
     footer {
       flex-direction: column;
       align-items: center;
---
+++ src/components/project.svelte
@@ -0,0 +1,144 @@
+<script>
+  const { url, name, year, description, route } = $props();
+</script>
+
+<section class="project">
+  <div class="top">
+    <a class="redirect" href={url} target="_blank" rel="noopener noreferrer">
+      <h1 class="name">{name}</h1>
+      <svg
+        class="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        ><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg
+      >
+    </a>
+    <p class="year">{year}</p>
+  </div>
+  <div class="middle">
+    <div class="img-placeholder"></div>
+    <!-- TODO: dynamic pills -->
+    <div class="pills"></div>
+  </div>
+  <div class="bottom">
+    <div class="description">
+      {description}
+    </div>
+    <a href={route}>&gt;&gt; Read Case Study</a>
+  </div>
+</section>
+
+<style>
+  .project {
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+  .project a {
+    color: black;
+  }
+  .project .redirect {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  .project .name {
+    font-size: 14px;
+    font-weight: 400;
+  }
+  .project .icon {
+    height: 18px;
+  }
+  .project .top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: "departure-mono", monospace;
+    text-transform: uppercase;
+  }
+  .project .year {
+    font-family: "departure-mono", monospace;
+    font-size: 14px;
+  }
+  .project .middle {
+    width: 100%;
+  }
+  .project .img-placeholder {
+    width: 100%;
+    height: 40rem;
+    background: var(--color-gray-100);
+    border: 1px dashed var(--color-gray-400);
+  }
+  .project .bottom {
+    display: grid;
+    grid-template-columns: 1fr 200px;
+    gap: 2rem;
+  }
+  .project .description {
+    max-width: 560px;
+    font-family: "source-serif-4", serif;
+    font-size: 14px;
+  }
+  .project:nth-of-type(2) .description {
+    max-width: 400px;
+  }
+  .project:nth-of-type(3) .description {
+    max-width: 360px;
+  }
+  .project:last-of-type .description {
+    max-width: 340px;
+  }
+  .project .bottom a {
+    justify-self: end;
+    align-self: center;
+    font-family: "departure-mono", monospace;
+    font-size: 14px;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+
+  @media (max-width: 1000px) {
+    .project .img-placeholder {
+      height: 30rem;
+    }
+  }
+
+  @media (max-width: 650px) {
+    .project .img-placeholder {
+      height: 20rem;
+    }
+    .project .bottom {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+    .project .bottom a {
+      align-self: start;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .project .img-placeholder {
+      height: 15rem;
+    }
+  }
+
+  @media (max-width: 420px) {
+    .project .img-placeholder {
+      margin: 0 -3rem;
+      width: auto;
+      border-left: none;
+      border-right: none;
+    }
+  }
+</style>
